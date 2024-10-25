8035cef54f40f421b456a5d1a02eea3d40f870c0
карина <@DESKTOP-FP9I0KA>
2024-10-25 13:11:22 +0300

    Add project files.

---
+++ best-time-to-buy-and-sell-stock/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace best_time_to_buy_and_sell_stock
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int minPrice = int.MaxValue;
+            int maxProfit = 0;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < minPrice)
+                {
+                    minPrice = prices[i];
+                }
+                else if (prices[i] - minPrice > maxProfit)
+                {
+                    maxProfit = prices[i] - minPrice;
+                }
+            }
+
+            return maxProfit;
+        }
+
+        public static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            int[] prices = { 7, 1, 5, 3, 6, 4 };
+            int maxProfit = solution.MaxProfit(prices);
+            Console.WriteLine($"Максимальная прибыль: {maxProfit}"); 
+        }
+    }
+}
